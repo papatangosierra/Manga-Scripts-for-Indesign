2e0f6702f7030151ff4ef6683d19b018ab57a945
Paul Starr <pstarr@nymprh31410.us.randomhouse.com>
2019-12-03 16:11:25 -0500

    Update Collect Dialog Boxes.jsx

--- Collect Dialog Boxes.jsx
+++ Collect Dialog Boxes.jsx
@@ -1,11 +1,11 @@
 ﻿// This collects text from text boxes on the 'Dialog' layer
 
-function returnDialogStructure () {
+function getTextStructure () {
     var dialogPages = []; // dialogPages will be an array of arrays. Each sub-array will contain zero or more strings, each string corresponding to a single dialog bubble
     for (var i = 0; i < app.activeDocument.pages.length; i++) { // loop over pages
         dialogPages.push([]); // Add an empty array for every page we iterate over. 
         for (var j = 0; j < app.activeDocument.pages[i].textFrames.length; j++) { // loop over textFrames
-            if (app.activeDocument.pages[i].textFrames[j].itemLayer.name == 'text') { // If a textFrame is on the dialog layer
+            if (app.activeDocument.pages[i].textFrames[j].itemLayer.name == 'Text') { // If a textFrame is on the text layer
                 dialogPages[i].push(app.activeDocument.pages[i].textFrames[j].contents)
             }
         }
